f7adfd4d1fc22f291306508418147cabefddced5
Talha Jamil <jk9224090@gmail.com>
2025-09-28 10:15:30 +0500

    utils code discusiom

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import dotenv from "dotenv";
  dotenv.config({
     path:'.env'
  })
- connectDB();
+ connectDB()
+ .then(()=>{
+   app.listen(process.env.PORT,()=>{
+    console.log(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at code ${process.env.PORT}`);
+    
+    })
+ })
+
+ .catch  ((error)=>{
+    console.log("MONGODB CONNECTION ERROR",error) ;
+ })
+})
+ 
 //  import express from "express"
 //  const app= express();
 
@@ -23,4 +36,4 @@ import dotenv from "dotenv";
 //         console.log(error);
 //         throw error;
 //     }
-// })()
+// })();
---
+++ utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error{
+    constructor(
+       statusCode,
+       message="internal server error",
+       errors=[],
+       stack=""
+    ){
+        super(message)
+        this.data=null
+        this.statusCode=statusCode
+        this.message=message
+        this.success=false
+        this.errors=errors
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+export {ApiError}
